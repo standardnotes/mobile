3e5194131ff0da4cbffde2a75fd42a12d66024cb
Mo Bitar <me@bitar.io>
2018-05-01 11:40:19 -0500

    Decrypted export option

--- src/containers/account/OptionsSection.js
+++ src/containers/account/OptionsSection.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import {Alert} from 'react-native';
 import GlobalStyles from "../../Styles"
 import {TextInput, View} from 'react-native';
-
 import SectionHeader from "../../components/SectionHeader";
 import ButtonCell from "../../components/ButtonCell";
 import TableSection from "../../components/TableSection";
 import SectionedTableCell from "../../components/SectionedTableCell";
 import SectionedAccessoryTableCell from "../../components/SectionedAccessoryTableCell";
+import SectionedOptionsTableCell from "../../components/SectionedOptionsTableCell";
 
 export default class OptionsSection extends Component {
 
@@ -15,11 +16,23 @@ export default class OptionsSection extends Component {
     this.state = {loadingExport: false};
   }
 
-  onExportPress = () => {
+  onExportPress = (option) => {
+    let encrypted = option.key == "encrypted";
+    if(encrypted && !this.props.encryptionAvailable) {
+      Alert.alert('Not Available', "You must be signed in, or have a local passcode set, to generate an encrypted export file.", [{text: 'OK'}])
+      return;
+    }
     this.setState({loadingExport: true});
-    this.props.onExportPress(() => {
+    this.props.onExportPress(encrypted, () => {
       this.setState({loadingExport: false});
     })
+  }
+
+  exportOptions = () => {
+    return [
+      {title: "Encrypted", key: "encrypted", selected: this.props.encryptionAvailable},
+      {title: "Decrypted", key: "decrypted", selected: true}
+    ];
   }
 
   render() {
@@ -32,7 +45,15 @@ export default class OptionsSection extends Component {
           <ButtonCell first={true} leftAligned={true} title={`Sign out (${this.props.email})`} onPress={this.props.onSignOutPress} />
         }
 
-        <ButtonCell last={true} first={!this.props.signedIn} disabled={this.state.loadingExport} leftAligned={true} title={this.state.loadingExport ? "Preparing Data..." : "Export Data"} onPress={this.onExportPress} />
+        <SectionedOptionsTableCell
+          last={true}
+          first={!this.props.signedIn}
+          disabled={this.state.loadingExport}
+          leftAligned={true}
+          options={this.exportOptions()}
+          title={this.state.loadingExport ? "Preparing Data..." : "Export Data"}
+          onPress={this.onExportPress}
+        />
 
       </TableSection>
     );
