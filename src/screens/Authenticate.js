0c13c4cbe434c10f7cd92c731376d5417764beff
Mo Bitar <me@bitar.io>
2018-09-12 20:31:18 -0500

    Remove log

--- src/screens/Authenticate.js
+++ src/screens/Authenticate.js
@@ -34,7 +34,6 @@ export default class Authenticate extends Abstract {
   constructor(props) {
     super(props);
     this.authProps = props.authProps;
-    console.log("Initializing authenticate with auth props", this.authProps);
     this.state = {biometricsType: "touch", biometricsNoun: "Fingerprint"};
   }
 
