997b4a2c1cbf11b90423974f2b1fbe20e89a037e
Mo Bitar <me@bitar.io>
2019-01-14 13:18:36 -0600

    Update SFJS 0.3.32

--- src/style/ActionSheetWrapper.js
+++ src/style/ActionSheetWrapper.js
@@ -31,8 +31,6 @@ export default class ActionSheetWrapper {
   }
 
   handleActionSheetPress = (index) => {
-    console.log("handleActionSheetPress", index);
-
     let option = this.options[index];
     option.callback && option.callback(option);
   }
