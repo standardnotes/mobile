57a0ba33ea0fd14eb2fe82bfdaee28ddfb78627b
Johnny Almonte <johnny243@users.noreply.github.com>
2020-03-29 15:50:08 -0400

    More e2e testing on the Export Data feature

--- e2e/screens/options.spec.js
+++ e2e/screens/options.spec.js
@@ -1,3 +1,5 @@
+const moment = require('moment');
+
 describe('Export Data', () => {
   beforeEach(async () => {
     await device.reloadReactNative();
@@ -17,5 +19,21 @@ describe('Export Data', () => {
     await element(by.id('exportData-option-decrypted')).tap();
     await device.pressBack();
     await expect(element(by.id('exportData-title'))).toHaveText('Export Data');
+  });
+
+  it('should export decrypted notes', async () => {
+    await expect(element(by.id('exportData-option-decrypted'))).toBeVisible();
+    await element(by.id('exportData-option-decrypted')).tap();
+    await element(by.text('SAVE TO DISK')).tap();
+
+    const lastExportDate = new Date();
+
+    await element(by.text('DONE')).tap();
+    await expect(element(by.id('exportData-title'))).toHaveText('Export Data');
+
+    const formattedDate = moment(lastExportDate).format('lll');
+    const lastExportString = `Last exported on ${formattedDate}`;
+
+    await expect(element(by.id('lastExportDate-text'))).toHaveText(lastExportString);
   });
 });
