969ccde6ecc54d01d05cffd89357879f84c8ab81
Radek Czemerys <radko93@gmail.com>
2020-05-06 16:55:08 +0200

    Rename back to js

--- __tests__/index.android.js
+++ __tests__/index.android.js
@@ -1,6 +1,6 @@
 import 'react-native';
 import React from 'react';
-import Index from '../index.android.js';
+import Index from '../index.android';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
