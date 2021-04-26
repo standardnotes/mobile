19d535330d840e59b466ce0c3517059ad8b3edb4
Antonella Sgarlatta <antonella@standardnotes.org>
2021-04-26 14:19:41 -0300

    feat: remove editor loading message on ThemesActivated action (#415)
    * chore(version-snjs): 2.0.76
    
    * feat: remove loading editor message on ActivateThemes action

--- src/screens/Compose/Compose.tsx
+++ src/screens/Compose/Compose.tsx
@@ -181,11 +181,18 @@ export class Compose extends React.Component<{}, State> {
         identifier: 'component-view-' + Math.random(),
         areas: [ComponentArea.Editor],
         actionHandler: (currentComponent, action, data) => {
-          if (action === ComponentAction.SetSize) {
-            this.context?.componentManager!.handleSetSizeEvent(
-              currentComponent,
-              data
-            );
+          switch (action) {
+            case ComponentAction.SetSize:
+              this.context?.componentManager!.handleSetSizeEvent(
+                currentComponent,
+                data
+              );
+              break;
+            case ComponentAction.ThemesActivated:
+              this.setState({
+                loadingWebview: false,
+              });
+              break;
           }
         },
         contextRequestHandler: () => this.note,
@@ -432,7 +439,7 @@ export class Compose extends React.Component<{}, State> {
         this.setState({
           downloadingEditor: false,
         }),
-      100
+      200
     );
   };
 
