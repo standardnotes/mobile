1406b2294387745f4aeb73d28acba7a215e3147c
Antonella Sgarlatta <antonella@standardnotes.org>
2021-08-26 17:11:42 -0300

    feat: add switch styling for Android

--- src/components/SNSwitch.tsx
+++ src/components/SNSwitch.tsx
@@ -2,19 +2,29 @@ import React, { useContext } from 'react';
 import { Platform, Switch, SwitchProps } from 'react-native';
 import { ThemeContext } from 'styled-components';
 
-export type SNSwitchProps = Omit<SwitchProps, 'trackColor'>;
+export type SNSwitchProps = Omit<SwitchProps, 'trackColor' | 'thumbColor'>;
 
 export const SNSwitch: React.FC<SNSwitchProps> = props => {
   const theme = useContext(ThemeContext);
   return (
     <Switch
+      thumbColor={
+        Platform.OS === 'android'
+          ? props.value
+            ? theme.stylekitInfoColor
+            : theme.stylekitInfoContrastColor
+          : undefined
+      }
       trackColor={
         Platform.OS === 'ios'
           ? {
               false: theme.stylekitNeutralColor,
               true: theme.stylekitInfoColor,
             }
-          : undefined
+          : {
+              false: theme.stylekitShadowColor,
+              true: theme.stylekitShadowColor,
+            }
       }
       {...props}
     />
